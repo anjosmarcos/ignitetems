577b3e9e7ce4e6892c9de390dfa3d3be914fd777
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-02-25 15:41:42 -0300

    Componente GrupsCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,25 @@
 import { Header } from 'src/Components/Header';
 import { Container } from './styles';
+import { Hightlight } from 'src/Components/Highlight';
+import { GroupCard } from 'src/Components/GrupoCard';
 
 export default function Groups() {
   return (
     <Container>
       <Header showBackButton/>
+      <Hightlight
+        title="Turma"
+        subtitle="Escolha uma turma para participar"
+      />
+
+      <GroupCard
+        title="Turma A"
+      />
+      
+      <GroupCard
+        title="Turma B"
+      />
+
     </Container>
   );
 }
