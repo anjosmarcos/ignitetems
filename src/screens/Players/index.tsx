db9a42756e77e645d8cbb07cdc24c3c88a2c788e
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-02-26 11:13:14 -0300

    Componente Filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,19 +1,40 @@
 import { Header } from "src/Components/Header";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import { Hightlight } from "src/Components/Highlight";
 import { ButtonIcon } from "src/Components/ButtonIcon";
+import { Input } from "src/Components/Input";
+import { Filter } from "src/Components/Filter";
 
 export function Players() {
-    return(
+    return (
         <Container>
             <Header showBackButton />
 
-            <Hightlight 
+
+
+            <Hightlight
                 title="Nome da Turma"
                 subtitle="Adicione jogadores para criar turmas e partidas"
             />
-            
-            <ButtonIcon />
+
+
+
+            <Form>
+                <Input
+                    placeholder="Nome da turma"
+                    autoCorrect={false}
+                />
+
+                <ButtonIcon />
+
+            </Form>
+
+            <Filter
+                title="Todos"
+                isActive
+            />
+
+
 
         </Container>
     )
