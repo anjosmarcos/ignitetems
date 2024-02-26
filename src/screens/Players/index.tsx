4f8ca1b5a0a8bca411ba2da993645a630bf637bc
Marcos Alexandre <marcosanjos.pr@hotmail.com>
2024-02-26 11:35:00 -0300

    Listando filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,23 +1,24 @@
 import { Header } from "src/Components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { Hightlight } from "src/Components/Highlight";
 import { ButtonIcon } from "src/Components/ButtonIcon";
 import { Input } from "src/Components/Input";
 import { Filter } from "src/Components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 export function Players() {
+    const [team, setTeam] = useState('Time A')
+    const [players, setPlayers] = useState(['a', 'b'])
+
     return (
         <Container>
             <Header showBackButton />
-
-
 
             <Hightlight
                 title="Nome da Turma"
                 subtitle="Adicione jogadores para criar turmas e partidas"
             />
-
-
 
             <Form>
                 <Input
@@ -29,13 +30,25 @@ export function Players() {
 
             </Form>
 
-            <Filter
-                title="Todos"
-                isActive
-            />
+            <HeaderList>
+                <FlatList
+                    data={['Time A', 'Time B']}
+                    keyExtractor={(item) => item}
+                    renderItem={({ item }) => (
+                        <Filter
+                            title={item}
+                            isActive={item === team}
+                            onPress={() => setTeam(item)}
+                        />
+                    )}
+                    horizontal
 
+                />
+                <NumberOfPlayers>
+                   {players.length}
+                </NumberOfPlayers>
 
-
+            </HeaderList>
         </Container>
     )
 }
